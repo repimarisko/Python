83a0d9dd631666622cb1039fdc37bb036dc40350
repimarisko <92152453+repimarisko@users.noreply.github.com>
2022-02-27 22:53:41 +0700

    regresi linear
    menambahkan file

---
+++ regresilinear.py
@@ -0,0 +1,11 @@
+
+
+a = float(input("masukan angka 1 : "))
+b = float(input("masukan angka 2 : "))
+c = float(input("masukan angka 3 : "))
+
+c= a+b
+
+print("hasil : ",c)
+# create a data regresion calculator
+# create a line
